8f591ce23227684dc98132797b0e303d6a390d2e
gekishou <gonfuentes112@gmail.com>
2024-11-12 12:10:04 -0300

    feat: add new entry functionality

--- script.js
+++ script.js
@@ -6,6 +6,12 @@ const newButton = document.getElementById('new-button');
 const newDialog = document.getElementById('newBookDialog');
 const closeDialog = document.getElementById('close-dialog');
 
+const newTitle = document.getElementById('new-book-title');
+const newAuthor = document.getElementById('new-book-author');
+const newPages = document.getElementById('new-book-pages');
+const newAlreadyRead = document.getElementById('already-read-yes');
+const confirmButton = document.getElementById('confirm-submit-button');
+
 newButton.addEventListener('click', () => {
     newDialog.showModal();
 })
@@ -55,34 +61,49 @@ function createBookChildren(book) {
     return [bookTitle, bookAuthor, bookPages, bookRead];
 }
 
+function createNewEntry(book) {
+    const bookEntry = document.createElement('div');
+    bookEntry.classList.add('book');
 
+    const bookChildren = createBookChildren(book);
+    bookChildren.forEach((child) => {
+        bookEntry.appendChild(child);
+    });
+    bookDisplay.appendChild(bookEntry);
+    const newCheckbox = bookChildren[3].querySelector('.book-checkbox');
+    if (book.haveRead) {
+        newCheckbox.checked = true;
+        newCheckbox.dispatchEvent(new Event('change'));
+    }
+}
 
 function listBooks() {
     myLibrary.forEach((book) => {
-        const bookEntry = document.createElement('div');
-        bookEntry.classList.add('book');
-
-        const bookChildren = createBookChildren(book);
-        bookChildren.forEach((child) => {
-            bookEntry.appendChild(child);
-        });
-        bookDisplay.appendChild(bookEntry);
-        }
-
-    )
+        createNewEntry(book);
+        })
 }
 
-function addBook(title, author, pages) {
-    const newBook = new Book(title, author, pages, false);
+function addBook(title, author, pages, hasRead=false) {
+    const newBook = new Book(title, author, pages, hasRead);
     myLibrary.push(newBook);
 };
 
-addBook("book1withareallylongtitleaaaaaaaaaaaaaaaaaa", "author1", "pages1");
-addBook("book2", "author2", "pages2");
-addBook("book3", "author3", "pages3");
-addBook("book1withareallylongtitleaaaaaaaaaaaaaaaaaa", "author1", "pages1");
-addBook("book2", "author2", "pages2");
-addBook("book3", "author3", "pages3");
+function submitNewBook(title, author, pages, hasRead) {
+    const newBook = new Book(title, author, pages, hasRead);
+    addBook(newBook);
+    createNewEntry(newBook);
+}
+
+confirmButton.addEventListener('click', (event) => {
+    event.preventDefault();
+    submitNewBook(newTitle.value,
+                 newAuthor.value,
+                 newPages.value,
+                 newAlreadyRead.checked
+     );
+    newDialog.close();
+});
+
 addBook("book1withareallylongtitleaaaaaaaaaaaaaaaaaa", "author1", "pages1");
 addBook("book2", "author2", "pages2");
 addBook("book3", "author3", "pages3");
