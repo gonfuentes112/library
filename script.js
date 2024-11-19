d9063c6ae84850d5219d86f88a3188e790889d6e
gekishou <gonfuentes112@gmail.com>
2024-11-19 13:31:00 -0300

    feat: refactor into closure to remove global variables

--- script.js
+++ script.js
@@ -1,23 +1,3 @@
-const bookDisplay = document.getElementById('book-display');
-const newButton = document.getElementById('new-button');
-const newDialog = document.getElementById('newBookDialog');
-const closeDialog = document.getElementById('close-dialog');
-
-const newTitle = document.getElementById('new-book-title');
-const newAuthor = document.getElementById('new-book-author');
-const newPages = document.getElementById('new-book-pages');
-const newAlreadyRead = document.getElementById('already-read-yes');
-const confirmButton = document.getElementById('confirm-submit-button');
-
-newButton.addEventListener('click', () => {
-    newDialog.showModal();
-})
-
-closeDialog.addEventListener('click', () => {
-    newDialog.close();
-}
-)
-
 class Book {
     constructor(bookID, title, author, pages, haveRead) {
         this.id = bookID;
@@ -35,12 +15,6 @@ class Library {
     this.bookID = 0;
     };
 
-    listBooks() {
-        this.myLibrary.forEach((book) => {
-            createNewEntry(book);
-            })
-    };
-    
     addBook(title, author, pages, hasRead=false) {
         const newBook = new Book(this.bookID, title, author, pages, hasRead);
         this.bookID++;
@@ -61,104 +35,133 @@ class Library {
     
 }
 
-const myLibrary = new Library();
-myLibrary.addBook("book1withareallylongtitleaaaaaaaaaaaaaaaaaa", "author1", "pages1");
-myLibrary.addBook("book2", "author2", "pages2");
-myLibrary.addBook("book3", "author3", "pages3");
-myLibrary.listBooks();
+function libraryController() {
+    const bookDisplay = document.getElementById('book-display');
+    const newButton = document.getElementById('new-button');
+    const newDialog = document.getElementById('newBookDialog');
+    const closeDialog = document.getElementById('close-dialog');
 
-function removeBook(event) {
-    const target = event.target;
-    if (target.classList.contains('book-remove-button')) {
-        const bookId = event.currentTarget.dataset.bookId;
-        myLibrary.removeBookFromArray(bookId);
-        event.currentTarget.remove();
-    }
-}
-
-
-function toggleRead(event) {
-    const target = event.target;
-    if (target.classList.contains('readLabel')) {
-        const bookId = event.currentTarget.dataset.bookId;
-        myLibrary.toggleReadInArray(bookId);
-    }    
-}
-
-
-
-function createBookChildren(book) {
-    const bookTitle = document.createElement('div');
-    const bookAuthor = document.createElement('div');
-    const bookPages = document.createElement('div');
-    const bookRead = document.createElement('div');
-    const bookRemove = document.createElement('div');
-    const bookRemoveButton = document.createElement('button');
-    
-    bookTitle.innerText = `"${book.title}"`;
-    bookTitle.classList.add('book-title');
-
-    bookAuthor.innerText = `By: ${book.author}`;
-    bookAuthor.classList.add('book-author');
-
-    bookPages.innerText = `#Pages: ${book.pages}`;
-    bookPages.classList.add('book-pages');
-
-    bookRead.innerText = 'Already read?';
-    const readToggle = document.createElement('input');
-    readToggle.setAttribute('type', 'checkbox');
-    readToggle.setAttribute('id', `book-number-${book.id}`);
-    readToggle.classList.add('book-checkbox');
-    const readToggleLabel = document.createElement('label');
-    readToggleLabel.setAttribute('for', `book-number-${book.id}`);
-    readToggleLabel.classList.add('readLabel');
-
-    bookRead.appendChild(readToggle);
-    bookRead.appendChild(readToggleLabel);
-    bookRead.classList.add('book-have-read');
-
-
-    bookRemoveButton.innerText = "Remove";
-    bookRemoveButton.classList.add('book-remove-button');
-    bookRemove.classList.add('book-remove');
-    bookRemove.appendChild(bookRemoveButton);
-
-    return [bookTitle, bookAuthor, bookPages, bookRead, bookRemove];
-}
-
-function createNewEntry(book) {
-    const bookEntry = document.createElement('div');
-    bookEntry.classList.add('book');
-    bookEntry.dataset.bookId = book.id;
-
-    const bookChildren = createBookChildren(book);
-    bookChildren.forEach((child) => {
-        bookEntry.appendChild(child);
+    newButton.addEventListener('click', () => {
+        newDialog.showModal();
     });
-    bookDisplay.appendChild(bookEntry);
-    const newCheckbox = bookChildren[3].querySelector('.book-checkbox');
-    if (book.haveRead) {
-        newCheckbox.checked = true;
-        newCheckbox.dispatchEvent(new Event('change')); //manually trigger CSS change to modify style
+    
+    closeDialog.addEventListener('click', () => {
+        newDialog.close();
+    }
+    );
+    
+    const newTitle = document.getElementById('new-book-title');
+    const newAuthor = document.getElementById('new-book-author');
+    const newPages = document.getElementById('new-book-pages');
+    const newAlreadyRead = document.getElementById('already-read-yes');
+    const confirmButton = document.getElementById('confirm-submit-button');
+    
+    const myLibrary = new Library();
+    myLibrary.addBook("book1withareallylongtitleaaaaaaaaaaaaaaaaaa", "author1", "pages1");
+    myLibrary.addBook("book2", "author2", "pages2");
+    myLibrary.addBook("book3", "author3", "pages3");
+
+    function createNewEntry(book) {
+        const bookEntry = document.createElement('div');
+        bookEntry.classList.add('book');
+        bookEntry.dataset.bookId = book.id;
+    
+        const bookChildren = createBookChildren(book);
+        bookChildren.forEach((child) => {
+            bookEntry.appendChild(child);
+        });
+        bookDisplay.appendChild(bookEntry);
+        const newCheckbox = bookChildren[3].querySelector('.book-checkbox');
+        if (book.haveRead) {
+            newCheckbox.checked = true;
+            newCheckbox.dispatchEvent(new Event('change')); //manually trigger CSS change to modify style
+        }
+    
+        bookEntry.addEventListener('click', removeBook);
+        bookEntry.addEventListener('click', toggleRead);
     }
 
-    bookEntry.addEventListener('click', removeBook);
-    bookEntry.addEventListener('click', toggleRead);
-}
+    myLibrary.myLibrary.forEach((book) => {
+            createNewEntry(book);
+            });
 
-function submitNewBook(title, author, pages, hasRead) {
-    const newBook = myLibrary.addBook(title, author, pages, hasRead);
-    createNewEntry(newBook);
-}
 
-confirmButton.addEventListener('click', (event) => {
-    event.preventDefault();
-    submitNewBook(newTitle.value,
-                 newAuthor.value,
-                 newPages.value,
-                 newAlreadyRead.checked
-     );
-    newDialog.close();
-});
+    function removeBook(event) {
+        const target = event.target;
+        if (target.classList.contains('book-remove-button')) {
+            const bookId = event.currentTarget.dataset.bookId;
+            myLibrary.removeBookFromArray(bookId);
+            event.currentTarget.remove();
+        }
+    }
+    
+    function toggleRead(event) {
+        const target = event.target;
+        if (target.classList.contains('readLabel')) {
+            const bookId = event.currentTarget.dataset.bookId;
+            myLibrary.toggleReadInArray(bookId);
+        }    
+    }
+    
+    
+    
+    function createBookChildren(book) {
+        const bookTitle = document.createElement('div');
+        const bookAuthor = document.createElement('div');
+        const bookPages = document.createElement('div');
+        const bookRead = document.createElement('div');
+        const bookRemove = document.createElement('div');
+        const bookRemoveButton = document.createElement('button');
+        
+        bookTitle.innerText = `"${book.title}"`;
+        bookTitle.classList.add('book-title');
+    
+        bookAuthor.innerText = `By: ${book.author}`;
+        bookAuthor.classList.add('book-author');
+    
+        bookPages.innerText = `#Pages: ${book.pages}`;
+        bookPages.classList.add('book-pages');
+    
+        bookRead.innerText = 'Already read?';
+        const readToggle = document.createElement('input');
+        readToggle.setAttribute('type', 'checkbox');
+        readToggle.setAttribute('id', `book-number-${book.id}`);
+        readToggle.classList.add('book-checkbox');
+        const readToggleLabel = document.createElement('label');
+        readToggleLabel.setAttribute('for', `book-number-${book.id}`);
+        readToggleLabel.classList.add('readLabel');
+    
+        bookRead.appendChild(readToggle);
+        bookRead.appendChild(readToggleLabel);
+        bookRead.classList.add('book-have-read');
+    
+    
+        bookRemoveButton.innerText = "Remove";
+        bookRemoveButton.classList.add('book-remove-button');
+        bookRemove.classList.add('book-remove');
+        bookRemove.appendChild(bookRemoveButton);
+    
+        return [bookTitle, bookAuthor, bookPages, bookRead, bookRemove];
+    }
+    
+   
+    function submitNewBook(title, author, pages, hasRead) {
+        const newBook = myLibrary.addBook(title, author, pages, hasRead);
+        createNewEntry(newBook);
+    }
+    
+    confirmButton.addEventListener('click', (event) => {
+        event.preventDefault();
+        submitNewBook(newTitle.value,
+                     newAuthor.value,
+                     newPages.value,
+                     newAlreadyRead.checked
+         );
+        newDialog.close();
+    });
+}
+libraryController();
+
+
 
 
